bd1643d142f95c46181ef3e53d5f4149f8198e87
ClementCaillaud <34067765+ClementCaillaud@users.noreply.github.com>
2017-12-12 15:15:46 +0100

    Exercise 1 (2)

--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -1,9 +1,9 @@
 var expect = require('chai').expect;
 var shop_tools = require('./../src/shop-tools.js');
 
-describe('Tests de shop-tools', function()
+describe('Function getPrices()', function()
 {
-  it('Liste des prix', function()
+  it('Expect to return the correct object', function()
   {
     expect(shop_tools.getPrices()).to.eql({ banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 });
   })
